245df28b7025633077d04b0b72da172cd78ec6ee
Myles "Kameron" Dailey <mkd97@nau.edu>
2022-11-07 12:47:05 -0700

    Added comments
    Explanation of why patterns have a name and why it's important

--- manager/urls.py
+++ manager/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # giving the pattern a name value allows you to access the url from a template(html)
+    # Giving the pattern a name value allows you to refer to the url from a template(html)
+    # and in the views. Also, if the url path ever changes, you can use reverse( <url_name> ) to always get
+    # the correct path based on the name rather than the changing every reference to the url path iteself. 
+    # This follows DRY(DONT REPEAT YOURSELF). ie. Redirect(reverse('new_topping')) NOT Redirect(reverse('/new_topping'))
+    # Note: I forgot to implement this methodology in my views, TODO.
     path('', views.home, name="home"),
     path('toppings/', views.toppings, name="toppings"),
     path('pizzas/', views.pizzas, name="pizzas"),
